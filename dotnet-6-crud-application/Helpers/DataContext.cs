02849d30e915801df14dcda2e93d23f0703a877f
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-02-11 00:42:54 +0530

    Add files via upload

---
+++ dotnet-6-crud-application/Helpers/DataContext.cs
@@ -0,0 +1,22 @@
+namespace WebApi.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // in memory database used for simplicity.
+        options.UseInMemoryDatabase("UserDb");
+    }
+
+    public DbSet<User> Users { get; set; }
+}
